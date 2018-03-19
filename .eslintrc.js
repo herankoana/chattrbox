a11a76476bf0dc3792ea04c8acfb19de14b2f287
Anthony Hernandez <herankoana@csu.fullerton.edu>
2018-03-19 15:22:43 -0700

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "off",
+            "always"
+        ],
+        "no-undef": [
+            "off",
+            "always"
+        ]
+    }
+};
